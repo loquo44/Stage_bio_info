cb37febc8cd10ab8bdfd75f06fcc7c2b213abdd3
teo merenda <Teomerenda821@gmail.com>
2026-07-03 08:04:27 +0000

    tableau comparatif

--- base_de_donnees_complexportal/code_appelle.py
+++ base_de_donnees_complexportal/code_appelle.py
@@ -12,11 +12,13 @@ import requests
 import json
 from pathlib import Path
 
+output_file = Path(r"C:\Users\meren\Desktop\stage_info\complexportal_localisation_from_complexID.xlsx")
 json_dir=Path(r"C:\Users\meren\Desktop\stage_info\complexportal_json_localisation")
+
 json_dir.mkdir(exist_ok=True) # crée le dossier json_dir s'il n'existe pas déjà
 
-Complexe = "CPX-5977"
-organisme = 9606
+Complexe_id = "CPX-5977" # valeur à donner par l'utilisateur, peut être modifiée pour tester d'autres complexes
+organisme = 9606 # valeur à donner par l'utilisateur, peut être modifiée pour tester d'autres organismes
 
 print(f"recherche du nom de l'organisme : {organisme}")
 try: 
@@ -27,11 +29,10 @@ except Exception:
     nom_organisme = f"ID {organisme}" #solution de secour si le réseau est problématique
 print(f"Nom de l'organisme : {nom_organisme}")
 
-print(f"Recherche du complexe : {Complexe}")
+print(f"Recherche du complexe : {Complexe_id}")
 try:
-    comp_res = requests.get(f"https://www.ebi.ac.uk/complexportal/complexes/{Complexe}")
+    comp_res = requests.get(f"https://www.ebi.ac.uk/ebisearch/search?db=allebi&sortignorenull=true&query={Complexe_id}&size=15&requestFrom=masthead-black-bar")
     data_comp = comp_res.json()
 except Exception:
-    print(f"Erreur lors de la récupération du complexe : {Complexe}")
-    
-        
+    print(f"Erreur lors de la récupération du complexe : {Complexe_id}")
+
